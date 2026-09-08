ec48a6d9a6265fec1a3dca22339cb3db9560baef
lukasevicivan17-netizen <lukasevicivan17@gmail.com>
2026-09-08 22:34:57 +0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,25 @@
+print("Hello World")
+age = 10
+a = 1.15
+n = True
+lst = [1, 2, 3, 4, 5]
+tpl = (1, 2, 3, 4, 5, 6)
+dct = {'10': '1', '11': '2'}
+st = {1, 2, 3, 4, 5}
+
+x = 10
+y = 3
+print(age, type(age))
+print(a, type(a))
+print(n, type(n))
+print(lst, type(lst))
+print(tpl, type(tpl))
+print(dct, type(dct))
+print(st, type(st))
+print("Додавання:", x + y)
+print("Віднімання:", x - y)
+print("Множення:", x * y)
+print("Ділення:", x / y)
+print("Остача від ділення:", x % y)
+print("Цілочисельне ділення:", x // y)
+print("Піднесення до степеня:", x ** y)
